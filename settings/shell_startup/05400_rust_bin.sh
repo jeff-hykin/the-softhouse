8c8c35f70d7872407900de52926fb2a3ed1c0838
Jeff Hykin <jeff.hykin@gmail.com>
2021-05-07 18:54:18 -0500

    -

--- settings/shell_startup/05400_rust_bin.sh
+++ settings/shell_startup/05400_rust_bin.sh
@@ -1,2 +1,7 @@
 "$PROJECTR_FOLDER/settings/commands/.add_to_gitignore" "$PROJECTR_HOME/.cargo/bin"
 export PATH="$PATH:$PROJECTR_HOME/.cargo/bin"
+
+rustup toolchain install 1.51.0 # (43b129a20 2021-03-16)
+rustup default 1.51.0
+
+# rustc 1.51.0 (2fd73fabe 2021-03-23)
